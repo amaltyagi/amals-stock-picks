42ef6ee3318ad1c05aca861d8f1e755553867b54
amaltyagi <48773804+amaltyagi@users.noreply.github.com>
2023-07-31 03:41:09 -0500

    add financials sector

--- data.php
+++ data.php
@@ -5,7 +5,7 @@ $csvData = array_map('str_getcsv', file($csvFile));
 
 // Remove 'nan' values from the sector column
 $filteredData = array_filter($csvData, function ($row) {
-    return !empty($row[0]) && !in_array($row[0], ['VALUE INVESTING', 'GROWTH INVESTING', 'FINANCIALS']);
+    return !empty($row[0]) && !in_array($row[0], ['VALUE INVESTING', 'GROWTH INVESTING']);
 });
 
 // Convert the data to JSON format
